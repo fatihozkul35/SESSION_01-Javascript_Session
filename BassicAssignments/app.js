3ba8e40c5c442df3e146a55222fe7831913640a7
fatihozkul35 <fatihozkul3541@gmail.com>
2022-02-08 00:50:23 +0100

    bassic Assignments commit

---
+++ BassicAssignments/app.js
@@ -0,0 +1,29 @@
+// const sentence = "My name is Fatih and I am not a Terorist";
+// // const sentence = prompt("Enter your sentence please...");
+// let reversedStr = "";
+// for (let x of sentence) {
+//   reversedStr = x + reversedStr;
+// }
+// console.log(reversedStr);
+
+//********Reversing a Sentence */
+// const cumle = "hadi gel köyüze geri dönelim";
+
+// function ters(string) {
+//   return string.split("").reverse().join("");
+// }
+
+// console.log(ters(cumle));
+
+//****Finding fibonacci number */
+// function fibonacci(num) {
+//   if (num == 1) {
+//     return 1;
+//   } else if (num == 2) {
+//     return 2;
+//   } else {
+//     return fib(num - 1) + fib(num - 2);
+//   }
+// }
+
+// console.log(fib(12));
